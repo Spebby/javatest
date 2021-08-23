bc8289fd30eedaf3031c8e2f6b5c7fb6b27373c1
Spebby <47935791+Spebby@users.noreply.github.com>
2021-08-23 10:01:11 -0700

    Update Calculator.java
    still broken

--- CalculatorTest/Calculator.java
+++ CalculatorTest/Calculator.java
@@ -1,76 +1,75 @@
-// garbage calculator
-public class Calculator
+/* C# is cooler :)
+ * define the class
+*/
+import java.util.Scanner;
+public class Driver
 {   
-    public Calculator(float i0, float i1, CalculateMode calType)
-    {   
-        switch (calType) {
-            case Add:
-                Add(i0, i1);
-                break;
-            case Subtract:
-                Subtract(i0, i1);
-                break;
-            case Multiply:
-                Multiply(i0, i1);
-                break;
-            case Divide:
-                Divide(i0, i1);
-                break;
-            case Modulo:
-                Modulo(i0, i1);
-                break;
-            case GreaterThan:
-                GreaterThan(i0, i1);
-                break;
-        }
-    }
-    
-    public void Add(float i0, float i1)
+    // calculator mode
+    public static void main()
     {
-        float sum = i0 + i1;
-        System.out.println("The sum of " + i0 + " and " + i1 + " is " + sum);
-    }
-    
-    public void Subtract(float i0, float i1)
-    {
-        float sum = i0 - i1;
-        System.out.println("The difference of " + i0 + " and " + i1 + " is " + sum);
-    }
-    
-    public void Multiply(float i0, float i1)
-    {
-        float sum = i0 * i1;
-        System.out.println("The product of " + i0 + " and " + i1 + " is " + sum);
-    }
-    
-    public void Divide(float i0, float i1)
-    {
-        float sum = i0 / i1;
-        System.out.println("The quotient of " + i0 + " and " + i1 + " is " + sum);
-    }
-    
-    public void Modulo(float i0, float i1)
-    {
-        float sum = i0 % i1;
-        System.out.println("The remainder of " + i0 + " and " + i1 + " is " + sum);
-    }
-    
-    public void GreaterThan(float i0, float i1)
-    {
-        if(i0 > i1)
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Welcome to");
+        // Prints on the same line for effect.
+        // Wait of 1/4th of a second inbetween each .
+        System.out.print(".");
+        System.out.print(".");
+        System.out.print(".");
+        System.out.println(""); // Empty line for spacing.
+        // Wait a full second
+        // It may be worth adding waits between each line to invoke the feeling of an archaic machine.
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("    ><<              ><<                ><<    ><<<<       ><< <<  ");
+        System.out.println(" ><<   ><<           ><<                ><<  ><<    ><<  ><<    ><<");
+        System.out.println("><<          ><<     ><<   ><<<><<  ><< ><<><<        ><< ><<      ");
+        System.out.println("><<        ><<  ><<  ><< ><<   ><<  ><< ><<><<        ><<   ><<    ");
+        System.out.println("><<       ><<   ><<  ><<><<    ><<  ><< ><<><<        ><<      ><< ");
+        System.out.println(" ><<   ><<><<   ><<  ><< ><<   ><<  ><< ><<  ><<     ><< ><<    ><<");
+        System.out.println("   ><<<<    ><< ><<<><<<   ><<<  ><<><<><<<    ><<<<       ><< <<  ");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("");
+        
+        System.out.println("Enter the first Variable.");
+        double v1 = input.nextFloat();
+        // Code that prevents the rest of the code from being proccessed
+                           
+        System.out.println("Enter the final Variable.");
+        // Code that allows user to input Calculator Mode.
+        double v2 = input.nextFloat();
+        // Code that prevents the rest of the code from being proccessed
+        
+        System.out.println("Enter the Calculator Mode.");
+        System.out.println("Add(0), Subtract(1), Multiply(2), Divide(3), Remainder(4), Compair(5)");
+        // Code that allows user to input Calculator Mode.
+        int enumType = input.nextInt();
+        // Code that prevents the rest of the code from being proccessed
+        
+        
+        CalculateMode calType;
+        
+        switch(enumType)
         {
-            System.out.println(i0 + " is greater than " + i1);
-        }
-        else
-        {
-            if (i1 > i0)
-            {
-                System.out.println(i1 + " is greater than " + i0);
-            }
-            else
-            {
-                System.out.println(i0 + " and " + i1 + "equal each other");
-            }    
+            case 0:
+                Calculator calc = new Calculator(v1, v1, calType.Add);
+                break;
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            default:
+                System.out.println("ERROR! Invalid Type");
         }
     }
 }
+// Obama was here
