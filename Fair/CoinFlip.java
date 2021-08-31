374b41f048fa4d54aa3a51d95fdcfdf4c35606f4
Spebby <47935791+Spebby@users.noreply.github.com>
2021-08-31 10:01:25 -0700

    updt

---
+++ Fair/CoinFlip.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CoinFlip
+{
+    public void Initialize()
+    {
+        
+    }
+}
--- Fair/FT.java
+++ Fair/FT.java
@@ -1,3 +1,7 @@
+// In this game, the player enters a random number(s) (Birthday/rn perhaps?)
+// When they enter this, the computer generates a random value, and then
+// The computer compares said number to the player entered number
+// The computer will then give them a fortune.
 import java.util.Scanner;
 
 public class FT
