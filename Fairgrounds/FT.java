ab776a593a8b0389914f93984cb2a82b115e2941
Spebby <sebbiekatt20@gmail.com>
2021-08-31 22:18:01 -0700

    i love switches

--- Fairgrounds/FT.java
+++ Fairgrounds/FT.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class FT
 {
     Scanner scan = new Scanner(System.in);
+    FTManager ftMng;
+
     public void Initialize()
     {
         System.out.println("-------------------------------------------------------------------------");
@@ -22,11 +24,47 @@ public class FT
         System.out.println();
         System.out.println("Welcome to the Fortune Telling Tent, your destiny fortold!");
         System.out.println();
-        System.out.print("Enter a Number: ");
-        String c = scan.nextLine();
+        System.out.print("What month were you born on?");
+        String m = scan.nextLine();
 
+        System.out.print("And what day?");
+        int d = scan.nextInt();
 
+        ftMng = new FTManager(m, d);
+        m = scan.nextLine(); // bug with int and scanner
     }
 
+    // This is the fun part, the switch now has to compare the month the user inputed against the days avaliable. 
+    public void FortuneFinder()
+    {
+        int Nature; // This makes later logic easier, think of this as refering to a person's "nature"
+        // Simplifying this down to something more manageable. Otherwise this will be **way** too much work for a throw away game
+        // Will also likely simplify this down to 
+        switch(ftMng.GetMonth())
+        {
+            // Dividing this up by seasons.
+            case 12:
+            case 1:
+            case 2:
+                Nature = 0;
+                break;
+            case 3:
+            case 4:
+            case 5:
+                Nature = 1;
+                break;
+            case 6:
+            case 7:
+            case 8:
+                Nature = 2;
+                break;
+            case 9:
+            case 10:
+            case 11:
+                Nature = 3;
+                break;
+            // No default as there's already out of range protection done earlier.
+        }
+    }    
     
 }
--- Fairgrounds/FTManager.java
+++ Fairgrounds/FTManager.java
@@ -1,3 +1,103 @@
-public class FTManager {
-    
+public class FTManager 
+{
+    int Day;
+    int Month;
+    public FTManager(String m, int d)
+    {   
+        // Makes strings uppercase to simplify case fallthroughs.
+        String mU = m.toUpperCase();
+        m = null;
+
+        // Converts String into int for easier use.
+        switch(mU)
+        {
+            case "JANURARY":
+            case "JAN":
+                Month = 1;
+                break;
+            case "FEBURARY":
+            case "FEB":
+                Month = 2;
+                break;
+            case "MARCH":
+            case "MAR":
+                Month = 3;
+                break;
+            case "APRIL":
+            case "APR":
+                Month = 4;
+                break;
+            case "MAY":
+                Month = 5;
+                break;
+            case "JUNE":
+            case "JUN":
+                Month = 6;
+                break;
+            case "JULY":
+            case "JUL":
+                Month = 7;
+                break;
+            case "AUGUST":
+            case "AUG":
+                Month = 8;
+                break;
+            case "SEPTEMBER":
+            case "SEP":
+                Month = 9;
+                break;
+            case "OCTOBER":
+            case "OCT":
+                Month = 10;
+                break;
+            case "NOVEMBER":
+            case "NOV":
+                Month = 11;
+                break;
+            case "DECEMBER":
+            case "DEC":
+                Month = 12;
+                break;
+            default: // Funny guy protection
+                Month = 1;
+                break;
+        }
+
+        // Funny guy protection
+        if(d <= 0)
+            d = 1;
+        if(d > 31)
+            d = 31;
+
+        // Checks if the day is illegal for the month, if so, change it.
+        switch(Month)
+        {
+            case 2:
+                if(d >= 30)
+                    d = 29;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if(d >= 31)
+                    d = 30;
+                break;
+            default:
+                d = 30;
+                break;
+        }
+
+        Day = d;
+    }
+
+    public int GetDay()
+    {
+        return Day;
+    }
+
+    public int GetMonth()
+    {
+        return Month;
+    }
 }
