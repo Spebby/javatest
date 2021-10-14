87aaefecb8191f86faef310cc16a1f42443c2987
Spebby <47935791+Spebby@users.noreply.github.com>
2021-10-14 10:00:29 -0700

    Update Board.java

--- Spheres/Board.java
+++ Spheres/Board.java
@@ -5,15 +5,19 @@ import java.util.ArrayList;
 
 public class Board extends JPanel
 {
+    // Get the size of the screen.
     Dimension scrnSize = Toolkit.getDefaultToolkit().getScreenSize();
+    // Acount for Taskbar size.
     Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
     int tbH = scrnSize.height - winSize.height;
     
+    // Need a dynamically growing/shrinking Array, need an ArrayList
     ArrayList<Ball> balls = new ArrayList<Ball>();
 
     RGB BG;
     Vector2Int boardSize = new Vector2Int(scrnSize.width, scrnSize.height - tbH);
     
+    // SRP attempts
     Collision coll = new Collision();
     BallSpawner bs = new BallSpawner();
 
@@ -28,8 +32,8 @@ public class Board extends JPanel
     }
     
     public void Go()
-    {       
-        while(true)
+    {
+        while(true) // Never ends
         {
             for(int i = 0; i < balls.size(); i++)
                 balls.get(i).Move(boardSize);
@@ -40,18 +44,19 @@ public class Board extends JPanel
     }
 
     // this doesn't work
+    // Want to be able to type "1" and spawn 1 ball, "6" to spawn 6.
     public void keyPressed(KeyEvent key) 
     {
         if (key.getKeyCode() == KeyEvent.VK_SPACE) 
             bs.Spawn(balls, 1, this);
     }
     
-    public void paintComponent(Graphics page)
+    public void paintComponent(Graphics page) // Repaint the balls
     {
         super.paintComponent(page);
         for(int i = 0; i < balls.size(); i++)
                 balls.get(i).Draw(page);
     }
-
+    // encapsulation
     public Vector2Int getBSize() { return boardSize; }
 }
