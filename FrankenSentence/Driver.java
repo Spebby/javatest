332e247048d7c9ef012ffbc401ddc812c172b09c
Spebby <47935791+Spebby@users.noreply.github.com>
2021-11-10 10:04:48 -0800

    Create Driver.java

---
+++ FrankenSentence/Driver.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Driver
+{
+    public static void main()
+    {
+        Scanner scan = new Scanner(System.in);
+        ScentenceGenerator sg = new ScentenceGenerator();
+        int c = scan.nextInt();
+        scan.close();
+        sg.Generate(c);
+    }
+}
