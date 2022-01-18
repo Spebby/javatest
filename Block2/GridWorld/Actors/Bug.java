2ef3b4798d560b804f2a3667fb6ee4b4bbe84ce3
Spebby <sebbiekatt20@gmail.com>
2022-01-18 15:56:41 -0800

    yeye

---
+++ Block2/GridWorld/Actors/Bug.java
@@ -0,0 +1,86 @@
+/* AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann */
+ 
+package GridWorld.Actors;
+//import info.gridworld.grid.Grid;
+//import info.gridworld.grid.Location;
+import java.awt.Color;
+
+import GridWorld.Location;
+import GridWorld.Objects.Flower;
+import GridWorld.Viewport.Grid;
+
+// A Bug is an actor that can move and turn. It drops flowers as it moves.
+public class Bug extends Actor
+{
+    public Bug()
+    {
+        setColor(Color.RED);
+    }
+
+    /* Constructs a bug of a given color.
+     * @param bugColor the color for this bug */
+    public Bug(Color bugColor)
+    {
+        setColor(bugColor);
+    }
+
+    public void act()
+    {
+        if (canMove())
+            move();
+        else
+            turn();
+    }
+
+    public void turn()
+    {
+        setDirection(getDirection() + Location.HALF_RIGHT);
+    }
+
+    // Moves the bug forward, putting a flower into the location it previously occupied.
+    public void move()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (gr.isValid(next))
+            moveTo(next);
+        else
+            removeSelfFromGrid();
+        Flower flower = new Flower(getColor());
+        flower.putSelfInGrid(gr, loc);
+    }
+
+    /* Tests whether this bug can move forward into a location that is empty or contains a flower.
+     * @return true if this bug can move. */
+    public boolean canMove()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower);
+        // ok to move into empty location or onto flower
+        // not ok to move onto any other actor
+    }
+}
---
+++ Block2/GridWorld/_Launcher.java
@@ -0,0 +1,10 @@
+package GridWorld;
+
+// this class handles launching the program. Everything is initalized here.
+public class _Launcher 
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
